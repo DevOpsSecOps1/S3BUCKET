3084d5976f4a279314fb63dedafd49e583508c82
karthik-cherukuri <105440239+karthik-cherukuri@users.noreply.github.com>
2023-01-07 00:31:34 +0530

    Create creds.tf

---
+++ creds.tf
@@ -0,0 +1,10 @@
+AWS Config
+variable "aws_access_key" {
+default = "AKIAXZRFGMTHQFLGIDBB"
+}
+variable "aws_secret_key" {
+default = "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"
+}
+variable "aws_region" {
+default = "us-east-1"
+}
