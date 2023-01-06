1525cb6aab42f06f30c5bbec489f1260fbcf9118
karthik-cherukuri <105440239+karthik-cherukuri@users.noreply.github.com>
2023-01-07 00:48:23 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,10 @@
 resource "aws_s3_bucket" "onebucket" {
    bucket = "testingbuck2-s3-with-terraform"
    acl = "private"
+   block_public_acls       = true
+   block_public_policy     = true
+   ignore_public_acls      = true
+   restrict_public_buckets = true
    versioning {
       enabled = true
    }
@@ -9,11 +13,11 @@ resource "aws_s3_bucket" "onebucket" {
      Environment = "Test"
    }
 }
-resource "aws_s3_bucket_public_access_block" "app" {
-  bucket = aws_s3_bucket.app.id
+#resource "aws_s3_bucket_public_access_block" "app" {
+#  bucket = aws_s3_bucket.app.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
+ # block_public_acls       = true
+ # block_public_policy     = true
+ # ignore_public_acls      = true
+ # restrict_public_buckets = true
+#}
