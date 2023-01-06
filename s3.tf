2a306a8712223777ee15e96acdb5bd77935ab1fd
karthik-cherukuri <105440239+karthik-cherukuri@users.noreply.github.com>
2023-01-07 00:50:40 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -3,8 +3,6 @@ resource "aws_s3_bucket" "onebucket" {
    acl = "private"
    block_public_acls       = true
    block_public_policy     = true
-   ignore_public_acls      = true
-   #restrict_public_buckets = true
    versioning {
       enabled = true
    }
